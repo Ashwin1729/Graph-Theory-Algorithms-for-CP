63c439ba9d5cd7a98f4ce17b389564f215a0186f
Ashwin1729 <mathematicswroteus@gmail.com>
2022-11-04 18:02:05 +0530

    --

--- Center of a Star Graph.cpp
+++ Center of a Star Graph.cpp
@@ -3,8 +3,28 @@
 
 using namespace std;
 
+int findCenter(vector<vector<int>>& edges) {
+    int n = edges.size() + 1;
+    unordered_map<int, list<int>> graph;
+    
+    for(long unsigned int i=0; i<edges.size(); i++)
+    {
+        graph[edges[i][0]].push_back(edges[i][1]);
+        graph[edges[i][1]].push_back(edges[i][0]);
+    }
+    
+    for(auto x : graph)
+    {
+        if(x.second.size() == edges.size())
+        {
+            return x.first;
+        }
+    }
+    
+    return -1;
+}
+
 int main()
 {
-    cout << "hello world";
     return 0;
 }
